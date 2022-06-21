0599181404c6633e6ed9567cc21bc9a4787a8b26
tusharvidwans99 <tusharvidwans99@gmail.com>
2022-06-21 09:26:02 +0530

    [Tushar Vidwans] Converting Fahrenheit to celcius and vice-versa

---
+++ ConvertTemp.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TempratureConversion
+{
+    public class ConvertTemp
+    {
+        /// <summary>
+        /// This program will convert Fahrenheit temprature value to the celcius temprature vaulue.
+        /// </summary>
+        /// <param name="Fahrenheit"></param>
+        public static void FahrenheitToCelcius()
+        {
+            Console.Write("Enter the Fahrenheit Value: ");
+            int Fahrenheit = Convert.ToInt32(Console.ReadLine());
+            int celcius;
+            celcius = (Fahrenheit - 32) * 5 / 9;
+
+            Console.WriteLine($"{Fahrenheit} Degree Fahrenheit = {celcius} Degree Celsius");
+
+        }
+
+
+        /// <summary>
+        /// This program will convert Celcius temprature value to the Fahrenheit temprature value.
+        /// </summary>
+        /// <param name="celcius"></param>
+        public static void CelciusToFahrenheit()
+        {
+            Console.Write("Enter the Celcius Value: ");
+            int celcius = Convert.ToInt32(Console.ReadLine());
+            int Fahrenheit;
+            Fahrenheit = (celcius * 9 / 5) + 32;
+
+            Console.WriteLine($"{celcius} Degree Celcius = {Fahrenheit} Degree Fahrenheit");
+
+        }
+
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,33 @@
+﻿namespace TempratureConversion
+{
+    class Program
+    {
+        public static void Main(string[] args)
+        {
+        Repeat: Console.WriteLine("Press 1 to convert F -> C\nPress 2 to Convert C -> F");
+            int option = Convert.ToInt32(Console.ReadLine());
+
+              switch (option)
+            {
+                case 1:
+                    ConvertTemp.FahrenheitToCelcius();
+                    break;
+                case 2:
+                    ConvertTemp.CelciusToFahrenheit();
+                    break;
+                default:
+                    Console.WriteLine("Enter Valid Number (1 OR 2)");
+                    Console.WriteLine("Want to try again the press \'y\' else press enter");
+                    string anotherTry = Console.ReadLine(); 
+                    if(anotherTry == "y"){
+                        goto Repeat;
+
+                    }
+                    else
+                    {
+                        break;
+                    }
+            }
+        }
+    }
+}
